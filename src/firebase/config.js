c9675b902cf9a7a19d4eb6c95fe6a2aba8b4c2dc
akr5515 <akrangkon5515@gmail.com>
2021-06-13 16:19:16 +0600

    new one

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const Config = {
+  apiKey: "AIzaSyBiWHpEvPufFoUuVE2PcjJOQOEp3B9_gQc",
+  authDomain: "crwn-db-679f3.firebaseapp.com",
+  projectId: "crwn-db-679f3",
+  storageBucket: "crwn-db-679f3.appspot.com",
+  messagingSenderId: "624475205739",
+  appId: "1:624475205739:web:b8d1b25d0b2c9686be012e",
+  measurementId: "G-Q78JTHGNX2",
+};
+
+export default Config;
